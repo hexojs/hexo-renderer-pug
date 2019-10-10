755d6245fbde026c354ba2f7ff0e2c41f6446d01
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-10 09:09:30 +0100

    feat: drop Jade
    BREAKING CHANGE

--- test/index.js
+++ test/index.js
@@ -3,31 +3,7 @@
 const should = require('chai').should(); // eslint-disable-line
 
 describe('hexo-renderer-jade', () => {
-  const jade = require('../lib/jade');
   const pug = require('../lib/pug');
-
-  // Jade test cases
-  it('jade - default', () => {
-    const result = jade({
-      text: 'p Hello #{name}'
-    }, {
-      name: 'Hexo'
-    });
-
-    result.should.eql('<p>Hello Hexo</p>');
-  });
-
-  it('jade - compile', () => {
-    const render = jade.compile({
-      text: 'p Hello #{name}'
-    });
-
-    const result = render({
-      name: 'Hexo'
-    });
-
-    result.should.eql('<p>Hello Hexo</p>');
-  });
 
   // Pug test cases
   it('pug - default', () => {
